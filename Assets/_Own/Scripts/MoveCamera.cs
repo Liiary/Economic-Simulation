9d96240a89c8e6c1d3c91942eaf286283d94543a
Lia <alina.hw@web.de>
2018-07-19 21:05:35 +0200

    Walls + CameraMovementLimit

--- Assets/_Own/Scripts/MoveCamera.cs
+++ Assets/_Own/Scripts/MoveCamera.cs
@@ -13,5 +13,33 @@ public class MoveCamera : MonoBehaviour
 
         transform.position += new Vector3(xMovement, 0.0f, 0.0f);
         transform.position +=  new Vector3(0.0f, 0.0f, zMovement);
+
+        if(transform.position.x >= 59)
+        {
+            Vector3 pos = transform.position;
+            pos.x = 58;
+            transform.position = pos;
+        }
+
+        if(transform.position.x <= -2)
+        {
+            Vector3 pos = transform.position;
+            pos.x = -1;
+            transform.position = pos;
+        }
+
+        if(transform.position.z >= 59)
+        {
+            Vector3 pos = transform.position;
+            pos.z = 58;
+            transform.position = pos;
+        }
+
+        if (transform.position.z <= -2)
+        {
+            Vector3 pos = transform.position;
+            pos.z = -1;
+            transform.position = pos;
+        }
     }
 }
